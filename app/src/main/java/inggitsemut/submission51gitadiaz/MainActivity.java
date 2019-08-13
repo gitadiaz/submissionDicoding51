f40ceb5b281b8d5e556bfa6a548700a5311ecb04
Gita Diaz <30728116+gitadiaz@users.noreply.github.com>
2019-08-13 17:21:39 +0700

    about activity

--- app/src/main/java/inggitsemut/submission51gitadiaz/MainActivity.java
+++ app/src/main/java/inggitsemut/submission51gitadiaz/MainActivity.java
@@ -1,7 +1,11 @@
 package inggitsemut.submission51gitadiaz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.about:
+                Intent intentAbout = new Intent(MainActivity.this, AboutActivity.class);
+                startActivity(intentAbout);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
